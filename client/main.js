b861181f0e60ea3df7b21334d5ebc548519d8ce6
Michael Callender <michaelleon2810@gmail.com>
2019-02-07 15:43:47 -0400

    update

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
+import '../lib/collections.js';
 
 Template.Michael.events({
   'click .js-like'(event, instance) {
@@ -10,5 +10,33 @@ Template.Michael.events({
   },
 'click .js-dislike'(event, instance) {
   	console.log("OoF");
+
+  	},
+});
+
+
+
+Template.addProfile.events({ 
+ 'click .js-saveProfile'(event, instance) { 
+ 	var fName = $("#exampleModal input[name='FirstName']").val();
+ 	console.log("The first name is")
+
+ 	var lName = $("#exampleModal input[name='LasttName']").val();
+ 	console.log("The Last name is")
+
+ 	var Image = $("#exampleModal input[name='Image']").val();
+ 	console.log("The Image")
+
+
+var fName = $("#exampleModal input[name='FirstName']").val('');
+ 	console.log("The first name is")
+
+ 	var lName = $("#exampleModal input[name='LasttName']").val('');
+ 	console.log("The Last name is")
+
+ 	var Image = $("#exampleModal input[name='Image']").val('');
+ 	console.log("The Image")
+
+	$("#exampleModal").modal("hide");
   },
 });
